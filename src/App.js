c76f86024be52dd1d3806491f22bce4141e96a51
Jon Boucher <jonboucher.dev@gmail.com>
2022-08-29 14:23:55 -0400

    Add header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Header from './components/Header';
+
 function App() {
-  return <h1>Hello from the app component</h1>;
+  return (
+    <>
+      <Header />
+      <div className='container'></div>
+    </>
+  );
 }
 
 export default App;
