6738c29e205548e76f8aa4ec84b78bf3f8cbb33f
Jon Boucher <jonboucher.dev@gmail.com>
2022-08-30 10:29:27 -0400

    Can change card state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { useState } from 'react';
 import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
+import FeedbackStats from './components/FeedbackStats';
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(
+        feedback.filter((item) => {
+          return item.id !== id;
+        })
+      );
+    }
+  };
+
   return (
     <>
       <Header />
-      <div className='container'></div>
+      <div className='container'>
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
     </>
   );
 }
